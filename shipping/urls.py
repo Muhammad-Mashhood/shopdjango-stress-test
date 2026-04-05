109ed93b79df54cd2590d86a33232106e2b4a9bf
Muhammad-Mashhood <139689918+Muhammad-Mashhood@users.noreply.github.com>
2026-04-06 01:04:06 +0500

    Migrate shipping/urls.py to Django 5

--- shipping/urls.py
+++ shipping/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import re_path
 from shipping import views
 
 urlpatterns = [
-    url(r'^zones/$', views.ShippingZoneListView.as_view(), name='zone-list'),
-    url(r'^rates/$', views.ShippingRateListView.as_view(), name='rate-list'),
-    url(r'^shipments/$', views.ShipmentListView.as_view(), name='shipment-list'),
-    url(r'^shipments/(?P<pk>\d+)/track/$', views.update_tracking, name='shipment-track'),
+    re_path(r'^zones/$', views.ShippingZoneListView.as_view(), name='zone-list'),
+    re_path(r'^rates/$', views.ShippingRateListView.as_view(), name='rate-list'),
+    re_path(r'^shipments/$', views.ShipmentListView.as_view(), name='shipment-list'),
+    re_path(r'^shipments/(?P<pk>\d+)/track/$', views.update_tracking, name='shipment-track'),
 ]
