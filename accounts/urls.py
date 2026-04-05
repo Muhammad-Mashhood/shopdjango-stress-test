9bed648d1bcc7e2a3daa5c4744c138d28ed54d41
Muhammad-Mashhood <139689918+Muhammad-Mashhood@users.noreply.github.com>
2026-04-06 01:04:19 +0500

    Migrate accounts/urls.py to Django 5

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,17 +1,17 @@
 """
-accounts/urls.py - URL patterns for accounts app (Django 1.x url() style)
+accounts/urls.py - URL patterns for accounts app (Django 1.x re_path() style)
 """
-from django.conf.urls import url
+from django.urls import re_path
 from accounts import views
 
 urlpatterns = [
-    url(r'^register/$', views.UserRegistrationView.as_view(), name='register'),
-    url(r'^login/$', views.UserLoginView.as_view(), name='login'),
-    url(r'^logout/$', views.UserLogoutView.as_view(), name='logout'),
-    url(r'^profile/$', views.UserProfileView.as_view(), name='profile'),
-    url(r'^change-password/$', views.change_password, name='change-password'),
-    url(r'^verify/(?P<token>[a-f0-9]{64})/$', views.verify_email, name='verify-email'),
-    url(r'^addresses/$', views.AddressListCreateView.as_view(), name='address-list'),
-    url(r'^addresses/(?P<pk>\d+)/$', views.AddressDetailView.as_view(), name='address-detail'),
-    url(r'^activity/$', views.UserActivityListView.as_view(), name='activity'),
+    re_path(r'^register/$', views.UserRegistrationView.as_view(), name='register'),
+    re_path(r'^login/$', views.UserLoginView.as_view(), name='login'),
+    re_path(r'^logout/$', views.UserLogoutView.as_view(), name='logout'),
+    re_path(r'^profile/$', views.UserProfileView.as_view(), name='profile'),
+    re_path(r'^change-password/$', views.change_password, name='change-password'),
+    re_path(r'^verify/(?P<token>[a-f0-9]{64})/$', views.verify_email, name='verify-email'),
+    re_path(r'^addresses/$', views.AddressListCreateView.as_view(), name='address-list'),
+    re_path(r'^addresses/(?P<pk>\d+)/$', views.AddressDetailView.as_view(), name='address-detail'),
+    re_path(r'^activity/$', views.UserActivityListView.as_view(), name='activity'),
 ]
